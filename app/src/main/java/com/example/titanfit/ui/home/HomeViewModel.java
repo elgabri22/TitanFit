10bbf451d14c33197d71d4afd59199a8b76c65f2
Gabriel <gdenmor2001@g.educaand.es>
2025-04-13 09:14:40 +0200

    Inicio Proyecto

---
+++ app/src/main/java/com/example/titanfit/ui/home/HomeViewModel.java
@@ -0,0 +1,14 @@
+package com.example.titanfit.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+
+    public HomeViewModel() {
+
+    }
+
+}
